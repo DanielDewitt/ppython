e35fbf14c49820808eb7bc81d7c3871c1c606ff8
MarcSchaefer1 <mschaefer635@gmail.com>
2026-09-24 14:32:21 +0200

    Added serving class

--- ingredient.py
+++ ingredient.py
@@ -61,3 +61,37 @@ class Ingredient:
         print(f"Ballaststoffe: {self.fiber} g")
         print(f"Fett: {self.fat} g")
 
+class Serving:
+    def __init__(self, s_path, i_path, serving_name):
+
+        servings_data = pd.read_csv(s_path)
+        servings = servings_data.values.tolist()
+
+        for obj in servings:
+            if obj[0] == serving_name:
+                serving = obj[1:]
+
+        ingredient_list = {}
+
+        for i in range(0,len(serving),2):
+            if type(serving[i]) == str:
+                ingredient_name = serving[i]
+
+                ingredient_list[ingredient_name] = serving[i + 1]
+
+        self.carbs = 0
+        self.protein = 0
+        self.fat = 0
+        self.fiber = 0
+        self.sugar = 0
+        self.calories = 0
+
+        for ingredient in ingredient_list:
+            ingredientstats = Ingredient(i_path, ingredient)
+            self.carbs += ingredientstats.carbohydrates * ingredient_list[ingredient]
+            self.protein += ingredientstats.protein * ingredient_list[ingredient]
+            self.fat += ingredientstats.fat * ingredient_list[ingredient]
+            self.fiber += ingredientstats.fiber * ingredient_list[ingredient]
+            self.sugar += ingredientstats.sugar * ingredient_list[ingredient]
+            self.calories += ingredientstats.estimate_calories() * ingredient_list[ingredient]
+
